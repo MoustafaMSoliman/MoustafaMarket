b2d8a584221d0dcb5734ef56dc92952bb0a153b5
MoustafaMSeleman <moustafaseleman784@gmail.com>
2025-01-21 14:39:50 +0200

    BaseProjectSetupWithBaseDomainModels

---
+++ MoustafaMarket.Domain/Models/AggregateRoot.cs
@@ -0,0 +1,11 @@
+﻿namespace MoustafaMarket.Domain.Models;
+
+public abstract class AggregateRoot<TId> :Entity<TId>
+    where TId : notnull
+{
+    protected AggregateRoot(TId id)
+        : base(id)
+    {
+        
+    }
+}
---
+++ MoustafaMarket.Domain/Models/ValueObject.cs
@@ -0,0 +1,37 @@
+﻿namespace MoustafaMarket.Domain.Models;
+
+public abstract class ValueObject : IEquatable<ValueObject>
+{
+    public abstract IEnumerable<object> GetEqualityComponents();
+    public override bool Equals(object? obj)
+    {
+        if (obj is null || obj.GetType() != GetType())
+        {
+            return false;
+        }
+        //var valueObject =  (ValueObject) obj; //it will throw InvalidCastException
+        var valueObject = obj as ValueObject; // it will be null -safe casting-
+
+        if (valueObject is null)
+        {
+            return false;
+        }
+        return GetEqualityComponents().SequenceEqual(valueObject.GetEqualityComponents());
+    }
+
+    public override int GetHashCode()
+    {
+        return GetEqualityComponents()
+          .Select(x => x?.GetHashCode() ?? 0)
+          .Aggregate((x, y) => x ^ y);
+    }
+
+    public bool Equals(ValueObject? other)
+    {
+        return Equals((object?)other);
+    }
+
+    public static bool operator ==(ValueObject left, ValueObject right){ return Equals(left,right); }
+    public static bool operator !=(ValueObject left, ValueObject right) { return !Equals(left, right); }
+
+}
